af61160bee0d313fcc3d1fb1b6444ea824e201e6
Pinnacle96 <binary.rln@gmail.com>
2026-02-24 15:35:10 +0100

    Restore_Live_DB_Credentials

--- includes/dbconnection.php
+++ includes/dbconnection.php
@@ -6,7 +6,7 @@
 // }
 //local server
 
-$con=mysqli_connect("localhost", "root", "", "u499616432_adullamn_cams");
+$con=mysqli_connect("localhost", "u499616432_adullamn", "Rq;u54Y77#QFxx", "u499616432_adullamn_cams");
 if(mysqli_connect_errno()){
 echo "Connection Fail".mysqli_connect_error();
 }
